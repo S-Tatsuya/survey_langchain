da7ea3b976eb6ecf0cea8a5e6e33887cfb7cc0dd
Tatsuya Sakurai <tatsuya.sakurai@exmotion.co.jp>
2023-08-14 10:02:29 +0900

    feat: 変数で文字列を与えた場合の振る舞いを確認

--- tutorial_from_LangChain/src/quickstart.py
+++ tutorial_from_LangChain/src/quickstart.py
@@ -8,9 +8,23 @@ from langchain.llms import OpenAI
 llm = OpenAI(openai_api_key=os.getenv("OPENAI_API_KEY"))
 chat_model = ChatOpenAI()
 
+print("\033[41mLLMs First Prompts\033[0m")
 print("----- LLM -----")
 print(llm.predict("こんにちは"))
 print()
 
 print("----- Chat Model -----")
 print(chat_model.predict("こんにちは"))
+print()
+
+print("\033[42mString変数を与えた場合\033[0m")
+text = "カラフルな靴下を作成する会社にふさわしい名前を5つ教えてください"
+print(f"{text=}")
+
+print("----- LLM -----")
+print(llm.predict(text))
+print()
+
+print("----- Chat Model -----")
+print(chat_model.predict(text))
+print()
