20614341c2fe225c7c4b65e8acd6d69cd9602c9c
Tatsuya Sakurai <tatsuya.sakurai@exmotion.co.jp>
2023-08-14 10:15:23 +0900

    refactor: LLMのtutorialのcontentをわかるように関数化して説明を記載

--- tutorial_from_LangChain/src/quickstart.py
+++ tutorial_from_LangChain/src/quickstart.py
@@ -1,45 +1,53 @@
 import os
 
 from langchain.chat_models import ChatOpenAI
-
-# !LLMS tutorial
 from langchain.llms import OpenAI
 from langchain.schema import HumanMessage
 
-llm = OpenAI(openai_api_key=os.getenv("OPENAI_API_KEY"))
-chat_model = ChatOpenAI()
 
-print("\033[41mLLMs First Prompts: predict\033[0m")
-print("----- LLM -----")
-print(llm.predict("こんにちは"))
-print()
+def tutorial_of_llms():
+    """LLM, Chat Modelのpredict, predict_messagesの動きを確認する
+    1. 'こんにちは' を `predict` に与える
+    2. 'text'を変数に格納して `predict` に与える
+    3. '配列'を `predict_messages` に与える"""
+    llm = OpenAI(openai_api_key=os.getenv("OPENAI_API_KEY"))
+    chat_model = ChatOpenAI()
 
-print("----- Chat Model -----")
-print(chat_model.predict("こんにちは"))
-print()
+    print("\033[41mLLMs First Prompts: predict\033[0m")
+    print("----- LLM -----")
+    print(llm.predict("こんにちは"))
+    print()
 
-print("\033[42mString変数を与えた場合: predict\033[0m")
-text = "カラフルな靴下を作成する会社にふさわしい名前を5つ教えてください"
-print(f"{text=}")
+    print("----- Chat Model -----")
+    print(chat_model.predict("こんにちは"))
+    print()
 
-print("----- LLM -----")
-print(llm.predict(text))
-print()
+    print("\033[42mString変数を与えた場合: predict\033[0m")
+    text = "カラフルな靴下を作成する会社にふさわしい名前を5つ教えてください"
+    print(f"{text=}")
 
-print("----- Chat Model -----")
-print(chat_model.predict(text))
-print()
+    print("----- LLM -----")
+    print(llm.predict(text))
+    print()
 
-print("\033[43m配列を与えることのできるメソッド: predict_messages\033[0m")
-text = "カラフルな靴下を作成する会社にふさわしい名前を5つ教えてください"
-print(f"{text=}")
-messages = [HumanMessage(content=text)]
-print(f"Human Message={messages}")
+    print("----- Chat Model -----")
+    print(chat_model.predict(text))
+    print()
 
-print("----- LLM -----")
-print(llm.predict_messages(messages))
-print()
+    print("\033[43m配列を与えることのできるメソッド: predict_messages\033[0m")
+    text = "カラフルな靴下を作成する会社にふさわしい名前を5つ教えてください"
+    print(f"{text=}")
+    messages = [HumanMessage(content=text)]
+    print(f"Human Message={messages}")
 
-print("----- Chat Model -----")
-print(chat_model.predict_messages(messages))
-print()
+    print("----- LLM -----")
+    print(llm.predict_messages(messages))
+    print()
+
+    print("----- Chat Model -----")
+    print(chat_model.predict_messages(messages))
+    print()
+
+
+if __name__ == "__main__":
+    tutorial_of_llms()
