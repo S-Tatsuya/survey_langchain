4fd3d19b6b6de169a221848e0bba2feb14322df5
Tatsuya Sakurai <tatsuya.sakurai@exmotion.co.jp>
2023-08-14 10:10:15 +0900

    feat: 配列を与えるpredict_messagesを確認

--- tutorial_from_LangChain/src/quickstart.py
+++ tutorial_from_LangChain/src/quickstart.py
@@ -4,11 +4,12 @@ from langchain.chat_models import ChatOpenAI
 
 # !LLMS tutorial
 from langchain.llms import OpenAI
+from langchain.schema import HumanMessage
 
 llm = OpenAI(openai_api_key=os.getenv("OPENAI_API_KEY"))
 chat_model = ChatOpenAI()
 
-print("\033[41mLLMs First Prompts\033[0m")
+print("\033[41mLLMs First Prompts: predict\033[0m")
 print("----- LLM -----")
 print(llm.predict("こんにちは"))
 print()
@@ -17,7 +18,7 @@ print("----- Chat Model -----")
 print(chat_model.predict("こんにちは"))
 print()
 
-print("\033[42mString変数を与えた場合\033[0m")
+print("\033[42mString変数を与えた場合: predict\033[0m")
 text = "カラフルな靴下を作成する会社にふさわしい名前を5つ教えてください"
 print(f"{text=}")
 
@@ -27,4 +28,18 @@ print()
 
 print("----- Chat Model -----")
 print(chat_model.predict(text))
+print()
+
+print("\033[43m配列を与えることのできるメソッド: predict_messages\033[0m")
+text = "カラフルな靴下を作成する会社にふさわしい名前を5つ教えてください"
+print(f"{text=}")
+messages = [HumanMessage(content=text)]
+print(f"Human Message={messages}")
+
+print("----- LLM -----")
+print(llm.predict_messages(messages))
+print()
+
+print("----- Chat Model -----")
+print(chat_model.predict_messages(messages))
 print()
