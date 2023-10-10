af49e27f933a944c8c6640908184e9d0b3532c2e
Tatsuya Sakurai <tatsuya.sakurai@exmotion.co.jp>
2023-10-10 21:50:38 +0900

    feat: chat modelのサンプル2を実装

--- measurement/src/chat_model.py
+++ measurement/src/chat_model.py
@@ -1,5 +1,7 @@
 from langchain.chat_models import ChatOpenAI
-from langchain.schema import AIMessage, HumanMessage, SystemMessage
+
+# from langchain.schema import AIMessage  # AIMessageは応答のメッセージの型
+from langchain.schema import HumanMessage, SystemMessage
 
 chat = ChatOpenAI()
 
@@ -20,5 +22,16 @@ def messages_in_message_out_without_system_message():
     )
 
 
+def messages_in_message_out_with_system_message():
+    messages = [
+        SystemMessage(
+            content="You are a helpful assistant that translates English to Japanese."
+        ),
+        HumanMessage(content="I love programming"),
+    ]
+    print(chat(messages))
+
+
 if __name__ == "__main__":
-    messages_in_message_out_without_system_message()
+    # messages_in_message_out_without_system_message()
+    messages_in_message_out_with_system_message()
