7e2534a6594e5b1fbf86b0da7c2d32021115b025
Tatsuya Sakurai <tatsuya.sakurai@exmotion.co.jp>
2023-10-11 20:50:30 +0900

    refactor: 処理を関数化
    asyncの使い方をまだ理解できていない

--- measurement/src/token_counting.py
+++ measurement/src/token_counting.py
@@ -4,30 +4,44 @@ from langchain.callbacks import get_openai_callback
 from langchain.llms import OpenAI
 
 llm = OpenAI(temperature=0)
-with get_openai_callback() as cb:
-    llm("what is the square root of 4?")
 
-total_tokens = cb.total_tokens
-assert total_tokens > 0
-print(cb)
 
-with get_openai_callback() as cb:
-    llm("What is the square root of 4?")
-    llm("What is the square root of 4?")
+def single_communication():
+    with get_openai_callback() as cb:
+        llm("what is the square root of 4?")
 
-assert cb.total_tokens == total_tokens * 2
-print(cb)
+    print(cb)
 
-# with get_openai_callback() as cb:
-#     await asyncio.gather(
-#         *[llm.agenerate(["What is the square root of 4?"]) for _ in range(3)]
-#     )
 
-# assert cb.total_tokens == total_tokens * 3
+def multiple_communications(count=2):
+    with get_openai_callback() as cb:
+        for _ in range(count):
+            llm("what is the square root of 4?")
 
-# task = asyncio.create_task(llm.agenerate(["What is the square root of 4?"]))
-# with get_openai_callback() as cb:
-#     await llm.agenerate(["What is the square root of 4?"])
+    print(cb)
 
-# await task
-# assert cb.total_tokens == total_tokens
+
+async def async_multiple_communication():
+    with get_openai_callback() as cb:
+        await asyncio.gather(
+            *[llm.agenerate(["What is the square root of 4?"]) for _ in range(3)]
+        )
+
+    print(cb)
+
+
+async def task_multiple_communication():
+    task = asyncio.create_task(llm.agenerate(["What is the square root of 4?"]))
+    with get_openai_callback() as cb:
+        await llm.agenerate(["What is the square root of 4?"])
+
+    await task
+
+    print(cb)
+
+
+if __name__ == "__main__":
+    single_communication()
+    multiple_communications()
+    # async_multiple_communication()
+    # task_multiple_communication()
